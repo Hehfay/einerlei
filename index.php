fdb475eaecf917a383fa5232ac2703c05e5c0285
Hehfay <jeffrey.hartman4691@gmail.com>
2016-02-05 10:49:25 -0500

    Added pseudo code.

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Einerlei</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <p>Hello World.</p>
+    <?php
+      // Enter key
+      // if key is valid start quiz
+      // else tell user to buy a key
+    ?>
+  </body>
+</html>
