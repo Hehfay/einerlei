e184a313ec7d944a1481d5e9ad8d32b9ddc5c929
Isaac Meisner <ijmeisner@twc.com>
2016-02-11 19:11:03 -0500

    more edits to questionForm

--- questionForm.php
+++ questionForm.php
@@ -43,7 +43,102 @@ else
     $row = $result->fetch_array(MYSQLI_ASSOC);
     echo $row['questionNumber'].".) ";
     echo $row['question'];
-    echo "<input type='number name='age' min='1' max='100'>";
+    echo "<input type='number' name='age' min='1' max='100'>";
+    echo "<br>";
+
+    $row = $result->fetch_array(MYSQLI_ASSOC);
+    echo $row['questionNumber'].".) ";
+    echo $row['question'];
+    echo "<input type='number' name='yearsStudying' min='1' max='100'>";
+    echo "<br>";
+
+    $row = $result->fetch_array(MYSQLI_ASSOC);
+    echo $row['questionNumber'].".) ";
+    echo $row['question'];
+    echo "<input type='number' name='ageMusicalInstruction' min='1' max='100'>";
+    echo "<br>";
+
+    $row = $result->fetch_array(MYSQLI_ASSOC);
+    echo $row['questionNumber'].".) ";
+    echo $row['question'];
+    echo "<input type='number' name='ageMusicalStudying' min='1' max='100'>";
+    echo "<br>";
+
+    $row = $result->fetch_array(MYSQLI_ASSOC);
+    echo $row['questionNumber'].".) ";
+    echo $row['question'];
+    echo "<select name='currently'>";
+    echo "<option value='High school student uncertain of future role of music in life'> High school student uncertain of future role of music in life </option>";
+    echo "<option value='High school student looking toward career as a musician'>High school student looking toward career as a musician</option>";
+    echo "<option value='Undergraduate music minor'>Undergraduate music minor</option>";
+    echo "<option value='Undergraduate music major'>Undergraduate music major</option>";
+    echo "<option value='Masters music student'>Masters music student</option>";
+    echo "<option value='Doctoral music student'>Doctoral music student</option>";
+    echo "<option value='Amateur adult musician'>Amateur adult musician</option>";
+    echo "<option value='Part-time professional musician'>Part-time professional musician</option>";
+    echo "<option value='Full-time professional musician in early career stage'>Full-time professional musician in early career stage</option>";
+    echo "<option value='Full-time professional musician for at least ten years'>Full-time professional musician for at least ten years</option>";
+    echo "<option value='Other'>Other</option>";
+    echo "</select>";
+    echo "<br>";
+
+    $row = $result->fetch_array(MYSQLI_ASSOC);
+    echo $row['questionNumber'].".) ";
+    echo $row['question'];
+    echo "<select name='primaryInstrument'>";
+    echo "<option value='Woodwind'>Woodwind</option>";
+    echo "<option value='Brass'>Brass</option>";
+    echo "<option value='Strings'>Strings</option>";
+    echo "<option value='Percussion'>Percussion</option>";
+    echo "<option value='Keyboard'>Keyboard</option>";
+    echo "<option value='Vocalist'>Vocalist</option>";
+    echo "<option value='Conductor'>Conductor</option>";
+    echo "</select>"; 
+    echo "<br>";
+
+    $row = $result->fetch_array(MYSQLI_ASSOC);
+    echo $row['questionNumber'].".) ";
+    echo $row['question'];
+    echo "<select name='proPerformances'>";
+    echo "<option value='0'>0</option>";
+    echo "<option value='1'>1</option>";
+    echo "<option value='2'>2</option>";
+    echo "<option value='3'>3</option>";
+    echo "<option value='4'>4</option>";
+    echo "<option value='5'>5</option>";
+    echo "<option value='6'>6</option>";
+    echo "<option value='7'>7</option>";
+    echo "<option value='8'>8</option>";
+    echo "<option value='9'>9</option>";
+    echo "<option value='10'>10</option>";
+    echo "<option value='10+'>10+</option>";
+    echo "</select>"; 
+    echo "<br>";
+
+    $row = $result->fetch_array(MYSQLI_ASSOC);
+    echo $row['questionNumber'].".) ";
+    echo $row['question'];
+    echo "<select name='allPerformances'>";
+    echo "<option value='0'>0</option>";
+    echo "<option value='1'>1</option>";
+    echo "<option value='2'>2</option>";
+    echo "<option value='3'>3</option>";
+    echo "<option value='4'>4</option>";
+    echo "<option value='5'>5</option>";
+    echo "<option value='6'>6</option>";
+    echo "<option value='7'>7</option>";
+    echo "<option value='8'>8</option>";
+    echo "<option value='9'>9</option>";
+    echo "<option value='10'>10</option>";
+    echo "<option value='10+'>10+</option>";
+    echo "</select>"; 
+    echo "<br>";
+
+    $row = $result->fetch_array(MYSQLI_ASSOC);
+    echo $row['questionNumber'].".) ";
+    echo $row['question'];
+    echo "<input type='radio' name='medical' value='Yes'> Yes";
+    echo "<input type='radio' name='medical' value='No'> No";
     echo "<br>";
 
     $row = $result->fetch_array(MYSQLI_ASSOC);
@@ -54,56 +149,21 @@ else
     $row = $result->fetch_array(MYSQLI_ASSOC);
     echo $row['questionNumber'].".) ";
     echo $row['question'];
+    echo "<input type='radio' name='drugs' value='Yes'> Yes";
+    echo "<input type='radio' name='drugs' value='No'> No";
     echo "<br>";
 
     $row = $result->fetch_array(MYSQLI_ASSOC);
     echo $row['questionNumber'].".) ";
     echo $row['question'];
+    echo "<input type='radio' name='medication' value='Yes'> Yes";
+    echo "<input type='radio' name='medication' value='No'> No";
     echo "<br>";
 
     $row = $result->fetch_array(MYSQLI_ASSOC);
     echo $row['questionNumber'].".) ";
     echo $row['question'];
-    echo "<br>";
-
-    $row = $result->fetch_array(MYSQLI_ASSOC);
-    echo $row['questionNumber'].".) ";
-    echo $row['question'];
-    echo "<br>";
-
-    $row = $result->fetch_array(MYSQLI_ASSOC);
-    echo $row['questionNumber'].".) ";
-    echo $row['question'];
-    echo "<br>";
-
-    $row = $result->fetch_array(MYSQLI_ASSOC);
-    echo $row['questionNumber'].".) ";
-    echo $row['question'];
-    echo "<br>";
-
-    $row = $result->fetch_array(MYSQLI_ASSOC);
-    echo $row['questionNumber'].".) ";
-    echo $row['question'];
-    echo "<br>";
-
-    $row = $result->fetch_array(MYSQLI_ASSOC);
-    echo $row['questionNumber'].".) ";
-    echo $row['question'];
-    echo "<br>";
-
-    $row = $result->fetch_array(MYSQLI_ASSOC);
-    echo $row['questionNumber'].".) ";
-    echo $row['question'];
-    echo "<br>";
-
-    $row = $result->fetch_array(MYSQLI_ASSOC);
-    echo $row['questionNumber'].".) ";
-    echo $row['question'];
-    echo "<br>";
-
-    $row = $result->fetch_array(MYSQLI_ASSOC);
-    echo $row['questionNumber'].".) ";
-    echo $row['question'];
+    echo "<input type='text' name='goals'>";
     echo "<br>";
 
   }
@@ -126,7 +186,7 @@ else
     Select the appropriate number.<br>
   </p>";
 
-  for($j = 1; $j<=100; $j++)
+  for($j = 1; $j<=158; $j++)
   {
     $query = "select * from Question where questionNumber='".$j."b';";
     $result = $mysqli->query($query);
@@ -142,11 +202,11 @@ else
         echo $row["questionNumber"].".) ";
         echo $row["question"];
         echo "<br>";
-        echo "<input type='radio' name='".$questionNumber."' value='1'> 1";
-        echo "<input type='radio' name='".$questionNumber."' value='2'> 2";
-        echo "<input type='radio' name='".$questionNumber."' value='3'> 3";
-        echo "<input type='radio' name='".$questionNumber."' value='4'> 4";
-        echo "<input type='radio' name='".$questionNumber."' value='5'> 5";
+        echo "<input type='radio' name='".$row["questionNumber"]."' value='1'> 1";
+        echo "<input type='radio' name='".$row["questionNumber"]."' value='2'> 2";
+        echo "<input type='radio' name='".$row["questionNumber"]."' value='3'> 3";
+        echo "<input type='radio' name='".$row["questionNumber"]."' value='4'> 4";
+        echo "<input type='radio' name='".$row["questionNumber"]."' value='5'> 5";
         echo "<br>";
       }
       else
