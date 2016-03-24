09327a0314a6b8baf8faa7b4a8b9ee3d7fd02985
Hehfay <jeffrey.hartman4691@gmail.com>
2016-03-24 16:27:15 -0400

    Progress made on php string handling / sanitation of mysql queries.

--- likert/questionSubmit.php
+++ likert/questionSubmit.php
@@ -1,128 +1,111 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Performance Anxiety Diagostic Questionnaire</title>
-<link rel="stylesheet" href="../style.css">
-</head>
-<body>
 <?php
-$fname = "output.txt";
-$mode = "a+";
+$fname = 'output.mysql';
+$mode = 'a';
 $filehandle = fopen($fname, $mode);
 if($filehandle == false)
 {
-  echo "FUCK EVERYTHING";
+  echo "Error: File could not be written to.";
 }
 else
 {
-  echo "Output written to output.txt"."<br>";
-}
-if(isset($_POST))
-{
-  $i = 0;
-  foreach($_POST as $item => $value)
+  if(isset($_POST))
   {
-    $variable[$i] = $item;
-    $answers[$i] = $value;
-    $i++;
-  }
-  for($i = 0; $i < count($variable); $i++)
-  {
-    $temp = trim($variable[$i]);
-    $temp = str_split($temp);
-    for($k = 0; $k < count($temp); $k++)
+    foreach($_POST as $item => $value)
     {
-      if($temp[$k] == "_")
-      {
-        $temp[$k] = " ";
-      }
+      fwrite($filehandle, "$item $value\n");
     }
-    $variable[$i] = implode($temp);
-    $variable[$i] = trim($variable[$i]);
-    $variable[$i] = "'".$variable[$i].".'";
-  }
-  $mysqli = new mysqli("", "jhartma0", "jhartma0", "Quiz");
-  if($mysqli->connect_errno)
-  {
-    fwrite($filehandle, "Mysqli connect error.\n");
+    $mysqli = new mysqli("", "jhartma0", "jhartma0", "Quiz");
+    if($mysqli->connect_errno)
+    {
+      fwrite($filehandle, "Mysqli connect error $mysqli->connect_errno.\n");
+    }
+    else
+    {
+      $size = count($_POST) / 2;
+      fwrite($filehandle, "/* Connected okay. */\n");
+      fwrite($filehandle, "POST / 2 is $size\n");
+      for($i = 0; $i < $size; $i++)
+      {
+        $answers[$i] = $_POST["a$i"];
+        fwrite($filehandle, "$answers[$i]\n");
+      }
+      for($i = 0; $i < $size; $i++)
+      {
+        $query1 = "select id, score_inversely from LikertQuestion where question=";
+        $query2 = $_POST["q$i"];
+        $query2 = $mysqli->escape_string($query2);
+        $query2 = "'".$query2."';";
+        $query = $query1.$query2;
+        fwrite($filehandle, $query."\n");
+        $result = $mysqli->query($query);
+        $result = $result->fetch_assoc();
+        if($result['score_inversely'])
+        {
+          if($answers[$i] == 1)
+          {
+            $answers[$i] = 100;
+          }
+          elseif($answers[$i] == 2)
+          {
+            $answers[$i] = 75;
+          }
+          elseif($answers[$i] == 3)
+          {
+            $answers[$i] = 50;
+          }
+          elseif($answers[$i] == 4)
+          {
+            $answers[$i] = 25;
+          }
+          elseif($answers[$i] == 5)
+          {
+            $answers[$i] = 0;
+          }
+          elseif($answers[$i] == 6)
+          {
+            $answers[$i] = 0;
+          }
+        }
+        else
+        {
+          if($answers[$i] == 1)
+          {
+            $answers[$i] = 0;
+          }
+          elseif($answers[$i] == 2)
+          {
+            $answers[$i] = 25;
+          }
+          elseif($answers[$i] == 3)
+          {
+            $answers[$i] = 50;
+          }
+          elseif($answers[$i] == 4)
+          {
+            $answers[$i] = 75;
+          }
+          elseif($answers[$i] == 5)
+          {
+            $answers[$i] = 100;
+          }
+          elseif($answers[$i] == 6)
+          {
+            $answers[$i] = 0;
+          }
+        }
+        $query1 = "insert into LikertAnswer(license_id, question_id, answer) values(1, ".$result['id'].", ".$answers[$i].");";
+        fwrite($filehandle, $query1."\n");
+      }
+      echo "<script>";
+      echo "";
+      echo "</script>";
+      $result->free();
+      $mysqli->close();
+    }
   }
   else
   {
-    fwrite($filehandle, "Connected okay.\n");
-    for($i = 0; $i < count($answers); $i++)
-    {
-      $query = "select id, score_inversely from LikertQuestion where question=".$variable[$i].";";
-      fwrite($filehandle, $query."\n");
-      $result = $mysqli->query($query);
-      $result = $result->fetch_assoc();
-      if($result['score_inversely'])
-      {
-        if($answers[$i] == 1)
-        {
-          $answers[$i] = 100;
-        }
-        elseif($answers[$i] == 2)
-        {
-          $answers[$i] = 75;
-        }
-        elseif($answers[$i] == 3)
-        {
-          $answers[$i] = 50;
-        }
-        elseif($answers[$i] == 4)
-        {
-          $answers[$i] = 25;
-        }
-        elseif($answers[$i] == 5)
-        {
-          $answers[$i] = 0;
-        }
-        elseif($answers[$i] == 6)
-        {
-          $answers[$i] = 0;
-        }
-      }
-      else
-      {
-        if($answers[$i] == 1)
-        {
-          $answers[$i] = 0;
-        }
-        elseif($answers[$i] == 2)
-        {
-          $answers[$i] = 25;
-        }
-        elseif($answers[$i] == 3)
-        {
-          $answers[$i] = 50;
-        }
-        elseif($answers[$i] == 4)
-        {
-          $answers[$i] = 75;
-        }
-        elseif($answers[$i] == 5)
-        {
-          $answers[$i] = 100;
-        }
-        elseif($answers[$i] == 6)
-        {
-          $answers[$i] = 0;
-        }
-      }
-      $query1 = "insert into LikertAnswer(license_id, question_id, answer) values(1, ".$result['id'].", ".$answers[$i].");";
-      fwrite($filehandle, $query1."\n");
-    }
-    echo "<script>";
-    echo "";
-    echo "</script>";
-    $result->free();
-    $mysqli->close();
+    fwrite($filehandle, "/* POST array not set. */\n");
   }
+  fclose($filehandle);
 }
-else
-{
-  fwrite($filehandle, "POST array not set.\n");
-}
-?>
-</body>
-</html>
