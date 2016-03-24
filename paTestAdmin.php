1f883099b416fa818410ca8b72d806355ac63498
Titoriano <titoriano@gmail.com>
2016-03-24 14:22:29 -0400

    Updated Login page

--- paTestAdmin.php
+++ paTestAdmin.php
@@ -11,14 +11,14 @@
 <html lang = "en">
 
    <head>
-      <title>Tutorialspoint.com</title>
+      <title>Test Admin Page</title>
       <link href = "css/bootstrap.min.css" rel = "stylesheet">
 
       <style>
          body {
             padding-top: 40px;
             padding-bottom: 40px;
-            background-color: #ADABAB;
+            background-color: #adacac;
          }
 
          .form-signin {
@@ -75,7 +75,7 @@
 
    <body>
 
-      <h2>Enter Username and Password</h2>
+      <h2>Performance Anxiety Quiz Admin Page</h2>
       <div class = "container form-signin">
 
          <?php
@@ -84,11 +84,11 @@
             if (isset($_POST['login']) && !empty($_POST['username'])
                && !empty($_POST['password'])) {
 
-               if ($_POST['username'] == 'tutorialspoint' &&
-                  $_POST['password'] == '1234') {
+               if ($_POST['username'] == 'testAdmin' &&
+                  $_POST['password'] == 'example') {
                   $_SESSION['valid'] = true;
                   $_SESSION['timeout'] = time();
-                  $_SESSION['username'] = 'tutorialspoint';
+                  $_SESSION['username'] = 'testAdmin';
 
                   echo 'You have entered valid use name and password';
                }else {
@@ -105,16 +105,16 @@
             ?>" method = "post">
             <h4 class = "form-signin-heading"><?php echo $msg; ?></h4>
             <input type = "text" class = "form-control"
-               name = "username" placeholder = "username = tutorialspoint"
+               name = "username" placeholder = "username"
                required autofocus></br>
             <input type = "password" class = "form-control"
-               name = "password" placeholder = "password = 1234" required>
+               name = "password" placeholder = "password" required>
             <button class = "btn btn-lg btn-primary btn-block" type = "submit"
                name = "login">Login</button>
          </form>
 
          Click here to clean <a href = "logout.php" tite = "Logout">Session.
-         
+
       </div>
 
    </body>
