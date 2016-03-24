906cf7f6d5af37e6f085e68b654479fd444ff9ec
Titoriano <titoriano@gmail.com>
2016-03-24 15:35:10 -0400

    Message about stuff

---
+++ paQuizInfo.php
@@ -0,0 +1,37 @@
+<?php
+
+$username = $_POST["username"];
+$passwrod = $_POST["password"];
+
+echo $username;
+echo $password;
+
+$host = " ";
+$username = "tbroadus";
+$password = "tbroadus";
+$database = "Quiz";
+
+$mysqli = new mysqli($host, $username, $password, $database);
+$correctPass = false;
+if(mysqli_connect_errno()){
+  echo "<p>";
+  echo "Connect Error; ".mysqli_connect_errno();
+  echo "</p>";
+}
+else{
+  $query = " Select password from adminUser where username = ".$username;
+  $result = $mysqli->query($query);
+
+  if($result){
+      if($result != null && $result == hash("md5",$password)){
+        $correctPass = true;
+      }
+  }
+  else{
+    echo "<p>";
+    echo "Query Error: ".$mysqli->error;
+    echo "</p>";
+  }
+      $myqli->close();
+}
+?>
