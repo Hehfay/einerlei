a8da40cba246f901645907b0a429780442e88fd0
TeejStroyer27 <titoriano@gmail.com>
2016-03-30 20:22:29 -0400

    ehhh

--- paQuizInfo.php
+++ paQuizInfo.php
@@ -21,7 +21,8 @@ if(mysqli_connect_errno()){
   echo "</p>";
 }
 else{
-  $query = "Select password from admins where username = '".$logUsername."' ;";
+  //$query = "Select password from admins where username ='".$logUsername."' ;";
+  $query = "Select password from admins where username=user1 ;";
 
   echo "<br>".$query;
   $result = $mysqli->query($query);
