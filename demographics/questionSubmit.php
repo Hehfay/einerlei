f9a10320b4c08c637d36d380f977c25ab54d2d92
Hehfay <jeffrey.hartman4691@gmail.com>
2016-04-03 18:08:18 -0400

    Removed question 9 from tables. Fixed an apostrophe issue in likertScaleQuestions.json. Finished the likert part of the quiz ( as much as I could at least ) start to finish and inserted the answers into the DB. Looks pretty good on that end.

--- demographics/questionSubmit.php
+++ demographics/questionSubmit.php
@@ -2,10 +2,11 @@
 header("Location: ../likert/");
 session_start();
 $handle = fopen("output.mysql", "a");
-$query = "insert into License(licenseKey, active) values(".$_SESSION["userid"].", TRUE)";
+$query = $_SESSION["userid"]." has these variables:";
 fwrite($handle, $query."\n");
 foreach($_POST as $item => $value)
 {
   fwrite($handle, "$item => $value\n");
 }
 fwrite($handle, "====================================================\n");
+fclose();
