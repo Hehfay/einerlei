d0f486f34ff44f842283b565ac3e3c16913326fe
Isaac Meisner <ijmeisner@twc.com>
2016-04-14 17:09:49 -0400

    big moves

---
+++ question-edit/createDemographicsJSONfile.cc
@@ -0,0 +1,96 @@
+//-------------------------------------------------------
+//
+// A program to create a json file from the demographic
+// text file.
+//
+//-------------------------------------------------------
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  ifstream infile;
+  ofstream outfile;
+
+  infile.open("demographicQuestions.txt");
+  if(infile.fail())
+  {
+   cerr << "Error opening dem file!" << endl;
+   exit(EXIT_FAILURE);
+  }
+
+  outfile.open("temp.json");
+  if (outfile.fail())
+  {
+   cerr << "Error opening output file!" << endl;
+   exit(EXIT_FAILURE);
+  }
+
+  string line = "";
+  string holder = "";
+  string noComma = "";
+  string correctLine = "";
+  string question = "question";
+  int i = 0;
+
+  outfile << "{" << endl;
+
+  while(getline(infile, line))
+  {
+    if(i % 2 == 0)
+    {
+      line = '"' + question + line + '"' + ":";
+      outfile << line;
+      holder = line;
+    }
+    else
+    {
+      line = line + ",";
+      outfile << line << endl;
+      holder += line;
+    }
+    i++;
+  }
+
+  outfile << "}" << endl;
+  infile.close();
+  outfile.close();
+
+//-------------------------------------------------------
+
+  noComma = holder.substr(0, holder.size()-1);
+
+  infile.open("temp.json");
+  if(infile.fail())
+  {
+   cerr << "Error opening temp file!" << endl;
+   exit(EXIT_FAILURE);
+  }
+
+  outfile.open("../demographics/demographicQuestions.json");
+  if(outfile.fail())
+  {
+   cerr << "Error opening temp file!" << endl;
+   exit(EXIT_FAILURE);
+  }
+
+  while(getline(infile, line))
+  {
+    if(line == holder)
+    {
+      outfile << noComma << endl;
+    }
+    else{
+      outfile << line << endl;
+    }
+  }
+
+  infile.close();
+  outfile.close();
+
+  return 0;
+}
