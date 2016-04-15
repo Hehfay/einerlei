62bfae94b587a6960837fc5c1c919dc6bca2db9f
Hehfay <jeffrey.hartman4691@gmail.com>
2016-04-15 10:54:46 -0400

    Small styling changes.

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ session_start();
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<div id ="introduction">
   <p>Welcome to the home of the MPADQ, the companion assessment questionnaire for <em>Take Charge of Your Performance Anxiety: A Personalized Approach to Conquering Stage Fright</em> by Dr. Heather Nicole Winter.</p>
   <p>Enter your key below to start your assessment. Your key is the ten-digit code found on the insert of your print copy or sent to you via email after the purchase of your ebook or audio book format of <em>Take Charge of Your Performance Anxiety</em>.</p>
   <form action="keySubmit.php" method="post">
@@ -22,6 +23,7 @@ session_start();
     session_write_close();
   }
   ?>
-  <p>Don't have a key? Purchase one at <a href="http://www.einerleipublishing.com/einerlei_publishing_site_002.htm" target="_blank">einerleipublishing.com</a></p>
+  <p>Don't have a key? Purchase one at <a href="http://www.einerleipublishing.com/einerlei_publishing_site_002.htm" id="einerlei-link" target="_blank">einerleipublishing.com</a></p>
+</div>
 </body>
 </html>
